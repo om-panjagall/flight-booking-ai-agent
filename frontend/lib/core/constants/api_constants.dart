a53cdee9e2cd1b4d2db73a869d4f5cb1a12ae336
WIN10 <omkar.panjagall@gmail.com>
2026-08-16 18:24:47 +0530

    Updated

--- frontend/lib/core/constants/api_constants.dart
+++ frontend/lib/core/constants/api_constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   ApiConstants._();
 
-  static const String baseUrl = 'http://localhost:8000';
+  static const String baseUrl = 'http://192.168.31.192:8000';
 
   static const String airports = '/api/v1/airports/';
 
